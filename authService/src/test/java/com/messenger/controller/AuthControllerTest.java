e06b72732d59a13b7ccea6a704e85b21108077dd
Artyom <petrakovartyom@gmail.com>
2025-09-05 01:17:17 +0300

    add test ci/cd pipelines

--- authService/src/test/java/com/messenger/controller/AuthControllerTest.java
+++ authService/src/test/java/com/messenger/controller/AuthControllerTest.java
@@ -3,10 +3,12 @@ package com.messenger.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
--- authService/src/test/java/com/messenger/controller/HealthcheckControllerTest.java
+++ authService/src/test/java/com/messenger/controller/HealthcheckControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,12 +12,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class HealthcheckControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void healthCheck() throws Exception {
-        mockMvc.perform(get("api/v1/auth/healthcheck")).andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/auth/healthcheck")).andExpect(status().isOk());
     }
 }
